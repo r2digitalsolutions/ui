34bb382fac0f1964cc05ead8b929c568ad76ff14
Codeunic <rafael.gonzalez.1737@gmail.com>
2025-03-19 18:47:48 +0100

    Sfix

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,2 +1,4 @@
-export * as Alert from './ui/Alert/Alert.svelte';
-export * as TableList from './ui/TableList/TableList.svelte';
+import Alert from './ui/Alert/Alert.svelte';
+import TableList from './ui/TableList/TableList.svelte';
+
+export { Alert, TableList };
