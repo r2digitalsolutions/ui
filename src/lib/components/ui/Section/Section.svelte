cd811d1d45888763512cbcbfff1bb281aab3355e
Codeunic <rafael.gonzalez.1737@gmail.com>
2025-07-16 08:52:30 +0200

    docs(changeset): fix: section class

--- src/lib/components/ui/Section/Section.svelte
+++ src/lib/components/ui/Section/Section.svelte
@@ -4,6 +4,6 @@
 	const { children, ...props }: Props = $props();
 </script>
 
-<section class={['relative flex flex-col gap-2 py-32', props.class]}>
+<section class={['cu-section relative flex flex-col gap-2 py-32', props.class]}>
 	{@render children()}
 </section>
